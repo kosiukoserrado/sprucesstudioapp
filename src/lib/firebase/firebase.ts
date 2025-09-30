9dacd6c5288a0b5eb7a07c91f28b4c417d348c4d
Kosiuko Serrado <kosiserrado@gmail.com>
2025-09-30 02:13:23 +0000

    this is my project id for your knowledge:
    spruces-app-bff67

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -10,9 +10,9 @@ import { getFirestore } from "firebase/firestore";
 // IMPORTANT: Replace with your actual Firebase config values
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  authDomain: "spruces-app-bff67.firebaseapp.com",
+  projectId: "spruces-app-bff67",
+  storageBucket: "spruces-app-bff67.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
