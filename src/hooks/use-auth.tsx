af0bf4338c7c314fb999aeefb2fe430c00db2d02
Kosiuko Serrado <kosiserrado@gmail.com>
2025-09-30 02:15:48 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -60,7 +60,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
